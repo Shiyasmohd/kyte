0e570bbe218cc9f328302f00cb5c508b648da402
shiyasmohd <shiyasjaseena124@gmail.com>
2023-09-24 00:32:41 +0530

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,39 @@
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import Image from "next/image";
+import TestImg from "../public/hero-card-complete.jpeg"
 
 export default function Home() {
 	return (
-		<div>
-
+		<div className="flex justify-center">
+			<div className="max-w-screen-xl">
+				<h1 className="font-bold text-5xl leading-loose tracking-tighter my-6 gradient-txt-white">Projects</h1>
+				<div className="grid grid-cols-4 gap-4">
+					{
+						[1, 1, 1, 1].map(() => (
+							<Card className="py-4">
+								<CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+									<p className="text-tiny uppercase font-bold">Daily Mix</p>
+									<small className="text-default-500">12 Tracks</small>
+									<h4 className="font-bold flex w-full justify-between text-large">
+										Frontend Radio
+										<span>
+											$45
+										</span>
+									</h4>
+								</CardHeader>
+								<CardBody className="overflow-visible py-2">
+									<Image
+										alt="Card background"
+										className="object-cover rounded-xl"
+										src={TestImg}
+										width={270}
+									/>
+								</CardBody>
+							</Card>
+						))
+					}
+				</div>
+			</div>
 		</div>
 	);
 }
